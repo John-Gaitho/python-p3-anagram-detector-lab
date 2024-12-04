aaa2808a5b5d96b4481955269933a7294f2af50a
John Gaitho <jgaitho016@gmail.com>
2024-12-04 16:11:15 +0300

    complete code

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, candidates):
+        sorted_word = sorted(self.word)
+        return [candidate for candidate in candidates if sorted(candidate.lower()) == sorted_word]
